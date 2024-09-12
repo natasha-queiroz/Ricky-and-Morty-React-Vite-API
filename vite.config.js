030f614d74a084ba496744337877746a92fbb8be
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-09-12 19:46:40 -0300

    configuracoes de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ricky-and-Morty-API-React-Vite",
+  base: "/Ricky-and-Morty-React-Vite-API",
 });
